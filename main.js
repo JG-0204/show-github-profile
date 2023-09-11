6008e10b91d18f02d40c643c90b86d943e4a2b93
James <jmsgctn@gmail.com>
2023-09-11 14:11:11 +0800

    Add renderUser and renderUserNotFoundModal function
    if the show button is called and input has value call renderUser
    and check if getUser return a json file then display the user data int the webpage
    if getUser return 404 call renderUserNotFoundModal

--- main.js
+++ main.js
@@ -1,15 +1,78 @@
+const userInput = document.querySelector('#login-input');
+const showButton = document.querySelector('.btn');
+
+showButton.addEventListener('click', renderUser);
+
+async function renderUser() {
+   let user = await getUsers();
+   let html = '';
+
+   let segment = `
+   <div>
+      <img class="user-avatar"
+      src="${user.avatar_url}"
+      alt="user-avatar"/>
+   </div>
+
+   <section class="profile-info">
+      <h2 class="user-name">
+         <a href="${user.html_url}" target="_blank">
+            ${user.name ?? user.login}
+         </a>
+      </h2>
+      <p class="user-bio">${user.bio ?? 'This user has not included a bio.'}</p>
+   </section>
+
+   <div class="social-info">
+      <p class="user-twitter">${user.twitter_username ?? 'None'}</p>
+      <p class="user-followers">Followers: ${user.followers}</p>
+      <p class="user-following">Following: ${user.following}</p>
+   </div>
+
+   <section class="most-starred-repo">
+      <h2 class="repo-name">
+         <a href="#" target="_blank">sample-repo</a>
+      </h2>
+      <p class="repo-description">Lorem ipsum dolor sit.</p>
+
+      <div>
+         <p class="repo-stars">35</p>
+         <p class="repo-language">HTML</p>
+         <p class="repo-last-updated">2021-04-08</p>
+      </div>
+   </section>
+   
+   <div class="account-info">
+      <p class="user-public-repos">Public Repositories: ${user.public_repos}</p>
+      <p class="user-created-at">Date Created: ${user.created_at.slice(
+         0,
+         10
+      )}</p>
+   </div>`;
+
+   html += segment;
+
+   let container = document.querySelector('.data-container');
+   container.innerHTML = html;
+   container.style.display = 'grid';
+
+   userInput.value = '';
+}
+
 async function getUsers() {
-   let url = 'https://api.github.com/users/jg-0204';
+   if (userInput.value === '') return true;
+
+   let url = `https://api.github.com/users/${userInput.value}`;
+
    try {
       let res = await fetch(url);
-      if (res.status === 404) {
-         console.log('User Not Found.');
-      } else {
-         return res.json();
-      }
+      return res.status === 404 ? renderUserNotFoundModal() : res.json();
    } catch (err) {
       console.log(err);
    }
 }
 
-getUsers();
+function renderUserNotFoundModal() {
+   document.querySelector('dialog').showModal();
+   userInput.value = '';
+}
